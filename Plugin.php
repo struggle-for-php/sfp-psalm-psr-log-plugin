e7440aa467412967e2ff747e61ef7009bbcf394d
sasezaki <sasezaki@gmail.com>
2019-11-02 11:52:42 +0900

    init

---
+++ Plugin.php
@@ -0,0 +1,30 @@
+<?php
+namespace Sfp\Psalm\PsrLogPlugin;
+
+use SimpleXMLElement;
+use Psalm\Plugin\PluginEntryPointInterface;
+use Psalm\Plugin\RegistrationInterface;
+
+class Plugin implements PluginEntryPointInterface
+{
+    /** @return void */
+    public function __invoke(RegistrationInterface $psalm, ?SimpleXMLElement $config = null)
+    {
+        $psalm->addStubFile(__DIR__ . '/stubs/LoggerInterface.php');
+
+        // This is plugin entry point. You can initialize things you need here,
+        // and hook them into psalm using RegistrationInterface
+        //
+        // Here's some examples:
+        // 1. Add a stub file
+        // ```php
+        // $psalm->addStubFile(__DIR__ . '/stubs/YourStub.php');
+        // ```
+
+        // Psalm allows arbitrary content to be stored under you plugin entry in
+        // its config file, psalm.xml, so you plugin users can put some configiration
+        // values there. They will be provided to your plugin entry point in $config
+        // parameter, as a SimpleXmlElement object. If there's no configuration present,
+        // null will be passed instead.
+    }
+}
